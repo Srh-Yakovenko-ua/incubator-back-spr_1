0e27e52331c012f41dedb21110ffe7ecb88845c4
Serhii <s.yakovenko@hubmee.com>
2025-05-23 19:49:47 +0300

    fix:routes path

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -2,11 +2,13 @@ import express, { Express } from 'express';
 import { videosRouters } from './modules/videos/routers/videos.routers';
 import { testingRouters } from './testing/routers/testing.router';
 
+const INIT_ROUTE_HW_01 = '/hometask_01/api';
+
 export const setupApp = (app: Express) => {
   app.use(express.json());
 
-  app.use(`/videos`, videosRouters);
-  app.use(`/testing`, testingRouters);
+  app.use(`${INIT_ROUTE_HW_01}/videos`, videosRouters);
+  app.use(`${INIT_ROUTE_HW_01}/testing`, testingRouters);
 
   return app;
 };
