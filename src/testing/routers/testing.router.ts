729e6b12b40bb1fdeadf7d6f9b541d39830c7689
Serhii <s.yakovenko@hubmee.com>
2025-05-23 19:37:04 +0300

    fix:watch errors

--- src/testing/routers/testing.router.ts
+++ src/testing/routers/testing.router.ts
@@ -1,10 +1,12 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import { HttpStatuses } from '../../shared/enums/http-statuses';
 import { videosLocalDB } from '../../modules/videos/db/videos.local.db';
 
 export const testingRouters = Router({});
-
+const clearVideosDB = () => {
+  videosLocalDB.length = 0;
+};
 testingRouters.delete('/all-data', (req: Request, res: Response) => {
-  videosLocalDB = [];
+  clearVideosDB();
   res.sendStatus(HttpStatuses.NoContent);
 });
